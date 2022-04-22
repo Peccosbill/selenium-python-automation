7e6012ac2effd196c04c0c6d8db2c7786de0a73f
Peccosbill <jonathanmontini95@gmail.com>
2022-04-22 20:57:36 -0300

    Options de chrome

--- automationPage/searchItem.py
+++ automationPage/searchItem.py
@@ -1,4 +1,6 @@
+from requests import options
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import unittest
 import time
 from pageIndex import PageIndex
@@ -9,33 +11,38 @@ from pageItem import PageItem
 class SearchCases(unittest.TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome('drivers\chromedriver.exe')
+        option = Options()
+        # option.add_argument('start-maximized')
+        # option.add_argument('incognito')
+        option.add_argument('--headless')
+        # option.add_argument('--disable-gpu')
+        self.driver = webdriver.Chrome(
+            'drivers\chromedriver.exe', chrome_options=option)
         self.driver.get('http://automationpractice.com/index.php')
         self.driver.implicitly_wait(5)
+        # self.driver.maximize_window()
+        # self.driver.set_window_size(200, 240)
         self.indexPage = PageIndex(self.driver)
         self.itemsPage = PageItems(self.driver)
         self.itemPage = PageItem(self.driver)
-    
-    @unittest.skip('Not nedded now')
-    def test_search_no_elements(self):
-        self.indexPage.search('hola')
-        time.sleep(5)
-        self.assertEqual(self.itemsPage.return_no_element_text(),
-                         'No results were found for your search "hola"')
 
-    @unittest.skip('Not nedded now')
+    def test_search_no_elements(self):
+        try:
+            self.indexPage.search('pepe')
+            self.assertEqual(self.itemsPage.return_no_element_text(),
+                            'No results were found for your search "hola"')
+        except:
+            self.driver.save_screenshot('error.jpg')
+
+
     def test_search_find_dresses(self):
         self.indexPage.search('dress')
-        time.sleep(5)
         self.assertTrue('DRESS' in self.itemsPage.return_section_titles())
 
-    @unittest.skip('Not nedded now')
     def test_search_find_tshirts(self):
         self.indexPage.search('t-shirt')
-        time.sleep(5)
         self.assertTrue('T-SHIRT' in self.itemsPage.return_section_titles())
 
-    @unittest.skip('Not nedded now')
     def test_tarea(self):
         self.indexPage.search('t-shirt')
         self.itemsPage.select_color()
@@ -47,9 +54,7 @@ class SearchCases(unittest.TestCase):
     def test_selections(self):
         self.indexPage.search('t-shirt')
         self.itemsPage.select_by_text('Product Name: Z to A')
-        time.sleep(3)
         self.itemsPage.select_by_value('reference:asc')
-        time.sleep(3)
         self.itemsPage.select_by_index(4)
         time.sleep
 
